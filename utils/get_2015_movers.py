3f316f10b476fe95775cb7571641b98e1fb38d82
Rudy Huezo <rahuezo@ucdavis.edu>
2018-08-28 07:17:18 -0700

    Started looping through 2015 dbs.

---
+++ utils/get_2015_movers.py
@@ -0,0 +1,37 @@
+from database import Database
+
+import tkFileDialog as fd
+import os, csv, sys
+
+
+relevant_counties = [
+    '6021', '6097', '6041', '6017', '6023', '6093', '6115', '6033', '6049', 
+    '6063', '6103', '6057', '6113', '6089', '6067', '6105', '6045', '6011', 
+    '6015', '6095', '6035', '6061', '6007', '6005', '6091', '6101', '6055'
+]
+
+wd = fd.askdirectory(title="Choose folder with 2015 mover databases")
+
+if not wd: 
+    print "\nNo folder selected. Goodbye!\n"
+    sys.exit()
+
+databases_2015 = [os.path.join(wd, f) for f in os.listdir(wd) if '2015' in f]
+
+if not databases_2015: 
+    print "\nNo databases found. Goodbye!\n"
+    sys.exit()
+
+# Loop through 2015 databases
+
+for db_file in databases_2015: 
+    current_db = Database(db_file)
+
+    for fips in relevant_counties: 
+        results = current_db.select("SELECT user_id, tweet_text, fips, tweet_date FROM tweets WHERE fips={}".format(fips))
+
+        print "One result: ", results.fetchone()
+        
+
+
+    
