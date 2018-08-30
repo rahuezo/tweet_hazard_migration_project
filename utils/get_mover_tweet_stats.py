be601919f0b6b4da034512669166bcd6b8c96ebb
Rudy Huezo <rahuezo@ucdavis.edu>
2018-08-30 07:08:53 -0700

    Started get_mover_tweet_stats.

--- utils/get_mover_tweet_stats.py
+++ utils/get_mover_tweet_stats.py
@@ -2,3 +2,40 @@
 
 import tkFileDialog as fd
 import cPickle as pk
+
+
+
+
+
+mover_db_files = fd.askopenfilenames(title="Choose mover databases")
+
+if not mover_db_files: 
+    print "\nNo mover dbs selected. Goodbye!\n"
+    sys.exit()
+
+
+print "\nGrabbing mover tweet stats..."
+
+for i, mover_db_file in enumerate(mover_db_files): 
+    print "\t{} out of {}".format(i + 1, len(mover_db_files))
+
+    current_db = Database(db_file)
+
+    results = current_db.select(    
+        """
+        SELECT
+            user_id "User ID",
+            fips "Fips", 
+            tweet_date "Date", 
+            COUNT(tweet_text) "Total Tweets"
+        FROM
+        tweets
+        GROUP BY
+        user_id, fips, substr(tweet_date, 0, 11)
+        """
+    )
+
+    for result in results: 
+        print result
+
+print "\nFinished grabbing mover tweet stats..."
